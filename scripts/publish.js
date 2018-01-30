88c27cc6d87e21e1f6f7048aa65a861c6d29e798
thierryskoda <skodathierry@hotmail.ca>
2018-01-30 14:28:42 -0500

    Fix publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,7 +3,7 @@ const packageJsonFile = require('../package.json')
 
 const newPackageJsonFile = {
   ...packageJsonFile,
-  version: `0.0.${parseInt(packageJsonFile.version.split('.')[1]) + 1}`,
+  version: `0.0.${parseInt(packageJsonFile.version.split('.')[2]) + 1}`,
 }
 
 fs.writeFileSync('../package.json', JSON.stringify(newPackageJsonFile, null, 2))
