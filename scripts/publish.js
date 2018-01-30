a660e6012b2e5b1e28506930434f4e36a47359d1
thierryskoda <skodathierry@hotmail.ca>
2018-01-30 14:45:57 -0500

    Fix publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,6 +6,6 @@ const newPackageJsonFile = {
   version: `0.0.${parseInt(packageJsonFile.version.split('.')[2]) + 1}`,
 }
 
-fs.writeFileSync('../package.json', JSON.stringify(newPackageJsonFile, null, 2))
+fs.writeFileSync('package.json', JSON.stringify(newPackageJsonFile, null, 2))
 
 console.log('New npm version:', newPackageJsonFile.version)
